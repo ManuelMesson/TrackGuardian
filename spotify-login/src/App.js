54a0b45484046c4b1b4b02571c894a4f4daea18d
Manuel Messon-Roque <manuelmesson@gmail.com>
2023-12-24 09:17:29 +0000

    fix gitignore jesus christ

--- spotify-login/src/App.js
+++ spotify-login/src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
-class App extends React.Component {
-  handleLogin = () => {
+const App = () => {
+  const handleLogin = () => {
     window.location = 'http://localhost:8888/login';
   };
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <button onClick={this.handleLogin}>Login with Spotify</button>
-        </header>
-      </div>
-    );
-  }
+  return (
+    <div className="App" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: '#282c34' }}>
+      <header className="App-header">
+        <Button onClick={handleLogin} variant="success" size="lg">
+          Login with Spotify
+        </Button>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- spotify.js
+++ spotify.js
@@ -7,83 +7,96 @@ const app = express();
 
 const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
 const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
-// rest of your code...
 const REDIRECT_URI = 'http://localhost:8888/callback';
 const scopes = 'user-library-read';
 
 let access_token = null;
 
+const constructAuthUrl = () => {
+  return `https://accounts.spotify.com/authorize?response_type=code&client_id=${SPOTIFY_CLIENT_ID}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}`;
+};
+
+const fetchAccessToken = async (code) => {
+  const { data } = await axios({
+    url: 'https://accounts.spotify.com/api/token',
+    method: 'post',
+    data: qs.stringify({
+      grant_type: 'authorization_code',
+      code: code,
+      redirect_uri: REDIRECT_URI
+    }),
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    auth: {
+      username: SPOTIFY_CLIENT_ID,
+      password: SPOTIFY_CLIENT_SECRET
+    }
+  });
+
+  return data.access_token;
+};
+
 app.get('/login', (req, res) => {
-  res.redirect('https://accounts.spotify.com/authorize' +
-    '?response_type=code' +
-    '&client_id=' + SPOTIFY_CLIENT_ID +
-    '&scope=' + encodeURIComponent(scopes) +
-    '&redirect_uri=' + encodeURIComponent(REDIRECT_URI));
+  res.redirect(constructAuthUrl());
 });
 
 app.get('/callback', async (req, res) => {
-  const code = req.query.code || null;
+  const code = req.query.code;
+
+  if (!code) {
+    res.status(400).send('Error: Missing authorization code');
+    return;
+  }
 
   try {
-    const { data } = await axios({
-      url: 'https://accounts.spotify.com/api/token',
-      method: 'post',
-      data: qs.stringify({
-        grant_type: 'authorization_code',
-        code: code,
-        redirect_uri: REDIRECT_URI
-      }),
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      auth: {
-        username: SPOTIFY_CLIENT_ID,
-        password: SPOTIFY_CLIENT_SECRET
-      }
-    });
-
-    access_token = data.access_token;
+    access_token = await fetchAccessToken(code);
     res.send('Successfully authenticated, you can now visit /tracks');
   } catch (err) {
     console.error(err);
-    res.send('Error getting access token');
+    res.status(500).send('Error getting access token');
   }
 });
 
 app.get('/tracks', async (req, res) => {
-    if (!access_token) {
-      res.send('Not authenticated, please visit /login');
+  if (!access_token) {
+    res.send('Not authenticated, please visit /login');
+    return;
+  }
+
+  let offset = 0;
+  let tracks = [];
+  let hasNextPage = true;
+
+  while (hasNextPage) {
+    try {
+      const { data } = await axios({
+        url: `https://api.spotify.com/v1/me/tracks?offset=${offset}`,
+        method: 'get',
+        headers: {
+          'Authorization': `Bearer ${access_token}`,
+          'Accept':'application/json',
+          'Content-Type': 'application/json'
+        }
+      });
+
+      tracks = [...tracks, ...data.items.map(item => item.track.name)];
+
+      hasNextPage = data.next !== null;
+      offset += data.limit;
+    } catch (err) {
+      console.error(err);
+      res.send('Error getting tracks');
       return;
     }
-  
-    let offset = 0;
-    let tracks = [];
-    let hasNextPage = true;
-  
-    while (hasNextPage) {
-      try {
-        const { data } = await axios({
-          url: `https://api.spotify.com/v1/me/tracks?offset=${offset}`,
-          method: 'get',
-          headers: {
-            'Authorization': `Bearer ${access_token}`,
-            'Accept':'application/json',
-            'Content-Type': 'application/json'
-          }
-        });
-  
-        tracks = [...tracks, ...data.items.map(item => item.track.name)];
-  
-        hasNextPage = data.next !== null;
-        offset += data.limit;
-      } catch (err) {
-        console.error(err);
-        res.send('Error getting tracks');
-        return;
-      }
-    }
-  
-    res.send(tracks);
-  });
+  }
+
+  res.send(tracks);
+});
+
+app.get('/logout', (req, res) => {
+  access_token = null;
+  res.send('Successfully logged out, you can now visit /login');
+});
 
 app.listen(8888);
